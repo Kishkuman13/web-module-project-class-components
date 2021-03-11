13f07481d0baba0b005b97a0c71a1696cb8f2ebc
Kishkuman13 <73566523+Kishkuman13@users.noreply.github.com>
2021-03-10 21:11:56 -0700

    added form functionality and styles

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react'
+import Todo from './Todo'
+
+const TodoList = props => {
+  return (
+    <div className='todo-list'>
+      {props.todoList.map(item => (
+        <Todo key={item.id} todo={item} toggleCompleted={props.toggleCompleted} />
+      ))}
+      <button onClick={props.clearCompleted} className='clear-btn'>
+        Clear Completed
+            </button>
+    </div>
+  )
+}
+
+export default TodoList;
